42c4bd680f59a90f244e532e9b26c6af132727be
Administrator <Administrator@VUQIGMDTLNKVVXF>
2019-10-28 11:54:27 +0800

    对代码进行了修改

--- src/com/qa/servlet/SessionDemo.java
+++ src/com/qa/servlet/SessionDemo.java
@@ -13,6 +13,7 @@ public class SessionDemo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("此处进行了代码修改");
 		response.setContentType("text/html; charset=UTF-8");
 		HttpSession session = request.getSession();
 		session.setAttribute("name", "zhangsan");
